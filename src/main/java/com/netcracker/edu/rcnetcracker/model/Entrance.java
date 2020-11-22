658e9216e98259cd23900191a1787e2d78cca631
MaxRomaschenko <romashenkomaxim@ukr.net>
2020-11-22 15:03:36 +0200

    add class Ekey/Entrance/Logger/Role/Type

---
+++ src/main/java/com/netcracker/edu/rcnetcracker/model/Entrance.java
@@ -0,0 +1,49 @@
+package com.netcracker.edu.rcnetcracker.model;
+
+public class Entrance {
+	protected Long entrance_id;
+	protected Long type_id;
+	protected Long location_id;
+	protected Long role_id;
+	protected boolean isActive;
+
+	public Long getEntrance_id() {
+		return entrance_id;
+	}
+
+	public void setEntrance_id(Long entrance_id) {
+		this.entrance_id = entrance_id;
+	}
+
+	public Long getType_id() {
+		return type_id;
+	}
+
+	public void setType_id(Long type_id) {
+		this.type_id = type_id;
+	}
+
+	public Long getLocation_id() {
+		return location_id;
+	}
+
+	public void setLocation_id(Long location_id) {
+		this.location_id = location_id;
+	}
+
+	public Long getRole_id() {
+		return role_id;
+	}
+
+	public void setRole_id(Long role_id) {
+		this.role_id = role_id;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean active) {
+		isActive = active;
+	}
+}
---
+++ src/main/java/com/netcracker/edu/rcnetcracker/model/Type.java
@@ -0,0 +1,22 @@
+package com.netcracker.edu.rcnetcracker.model;
+
+public class Type {
+	protected Long type_id;
+	protected String value;
+
+	public Long getType_id() {
+		return type_id;
+	}
+
+	public void setType_id(Long type_id) {
+		this.type_id = type_id;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
