aee8b82a8cfc81516dce2ded7933536fd4c24598
vS-Fanther <50148623+vS-Fanther@users.noreply.github.com>
2021-01-07 13:21:44 +0200

    building

---
+++ src/main/java/com/netcracker/edu/rcnetcracker/model/Building.java
@@ -0,0 +1,19 @@
+package com.netcracker.edu.rcnetcracker.model;
+
+import com.netcracker.edu.rcnetcracker.db.annotations.Attribute;
+import com.netcracker.edu.rcnetcracker.db.annotations.ObjectType;
+
+@ObjectType(id = 7)
+public class Building extends BaseEntity {
+
+    @Attribute(id = 15)
+    protected Long number;
+
+    public Long getNumber() {
+        return number;
+    }
+
+    public void setNumber(Long number) {
+        this.number = number;
+    }
+}
